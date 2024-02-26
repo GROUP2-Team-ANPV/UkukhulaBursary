62c0f17d2771892ca39c5441bbfcfee810b9a03a
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-26 15:40:32 +0200

    add navigation functionality

--- shell-app/static/JS/Index.js
+++ shell-app/static/JS/Index.js
@@ -1,70 +1,114 @@
+import login_view from "./views/login_view.js";
+import university_application_view from "./views/university_application_view.js";
 
- import login_view from "./views/login_view.js";
- import university_application_view from "./views/university_application_view.js";
-
-
-
-
-const navigateTo = url =>{
-    history.pushState(null, null ,url);
-    router();
-}
-
-const router = async () =>{
-    const routes = [
-        {path: "/", view: university_application_view},
-       // {path: "/login", view:login_view},
-       // {path: "/settings", view:(() => console.log("Viewing Settings"))}
-
-    ];
-
-    // Test each route for potential match
-    const potentialMatches = routes.map(route =>{
-        return {
-            route: route,
-            isMatch: location.pathname === route.path
-        }
-    })
-
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
-    
-
-    if (!match) {
-        match ={
-            route: routes[0],
-            isMatch: true
-        };
-    }
-    const view = new match.route.view();
-    document.querySelector("#content").innerHTML = await view.getHtml();
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
 };
 
-window.addEventListener("popstate" , router);
-document.addEventListener("DOMContentLoaded",()=>{
-    let menuIcon = document.querySelector('#menu_icon');
-    let menuIconClose = document.querySelector('#menu_icon_close');
-    let navbar = document.querySelector('.sidebar');
+const router = async () => {
+  const routes = [
+    { path: "/", view: university_application_view },
+    // {path: "/login", view:login_view},
+    // {path: "/settings", view:(() => console.log("Viewing Settings"))}
+  ];
 
-        menuIcon.addEventListener('click', () => {
-            
-                menuIcon.style.display='none';
-                navbar.style.display = 'block';
-                menuIconClose.style.display='flex'
+  // Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
 
-            
-        });
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
 
-        menuIconClose.addEventListener('click', () => {
-            menuIconClose.style.display='none';
-                navbar.style.display = 'none';
-                menuIcon.style.display='flex'
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    };
+  }
+  const view = new match.route.view();
+  document.querySelector("#main-content").innerHTML = await view.getHtml();
+};
 
-        });
-    document.body.addEventListener("click" ,e =>{
-        if(e.target.matches("[data-link]")){
-            e.preventDefault();
-            navigateTo(e.target.href);
-        }
-    } )
-    router();
+window.addEventListener("popstate", router);
+document.addEventListener("DOMContentLoaded", () => {
+  let menuIcon = document.querySelector("#menu_icon");
+  let menuIconClose = document.querySelector("#menu_icon_close");
+  let navbar = document.querySelector(".sidebar");
+
+  menuIcon.addEventListener("click", () => {
+    menuIcon.style.display = "none";
+    navbar.style.display = "block";
+    menuIconClose.style.display = "flex";
+  });
+
+  menuIconClose.addEventListener("click", () => {
+    menuIconClose.style.display = "none";
+    navbar.style.display = "none";
+    menuIcon.style.display = "flex";
+  });
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+  router();
+});
+
+const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
+
+allSideMenu.forEach((item) => {
+  const li = item.parentElement;
+
+  item.addEventListener("click", function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
+});
+
+const menuBar = document.querySelector("#content nav .bx.bx-menu");
+const sidebar = document.getElementById("sidebar");
+
+menuBar.addEventListener("click", function () {
+  sidebar.classList.toggle("hide");
+});
+
+const searchButton = document.querySelector(
+  "#content nav form .form-input button"
+);
+const searchButtonIcon = document.querySelector(
+  "#content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector("#content nav form");
+
+searchButton.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault();
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchButtonIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchButtonIcon.classList.replace("bx-x", "bx-search");
+    }
+  }
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("hide");
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace("bx-x", "bx-search");
+  searchForm.classList.remove("show");
+}
+
+window.addEventListener("resize", function () {
+  if (this.innerWidth > 576) {
+    searchButtonIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
 });
