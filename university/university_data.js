e6df554a7abbe8393ed5ac2129d29c52412c0b31
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-27 08:21:17 +0200

    break the university data access into sub functions for ease of maintenance

--- university/university_data.js
+++ university/university_data.js
@@ -1,5 +1,3 @@
-const universityStudents = document.querySelector(".students");
-
 async function getUniversityData(id) {
   const response = await fetch(
     `http://localhost:5263/api/UniversityAdmin/GetUniversityAndTheirStudents?universityID=${id}`,
@@ -18,26 +16,40 @@ async function getUniversityData(id) {
   return data;
 }
 
-getUniversityData(1).then(
-  (data) =>
-    data.students.forEach((student) => {
-      const studentItem = document.createElement("li");
-      studentItem.classList.add("student");
-      const studentName = document.createElement("h3");
-      studentName.classList.add("student__name");
-      const applicationStatus = document.createElement("p");
-      applicationStatus.classList.add("student__status",student.applicationStatus.toLowerCase());
-      const bursaryAmount = document.createElement("p");
-      bursaryAmount.classList.add("student__amount");
+const universityStudents = document.querySelector(".students");
+const fundedStudents = document.querySelector(".funded");
 
-      studentName.textContent = `${student.firstName} ${student.lastName}`;
-      applicationStatus.textContent = student.applicationStatus;
-      bursaryAmount.textContent = student.amount;
-      studentItem.appendChild(studentName);
-      studentItem.appendChild(applicationStatus);
-      studentItem.appendChild(bursaryAmount);
-      universityStudents.appendChild(studentItem);
-      
-    })
-    
-);
+getUniversityData(1).then((data) => {
+  renderStudents(data.students);
+  fundedStudents.textContent = getFundedStudents(data.students);
+});
+
+function getFundedStudents(students) {
+  return students.filter((student) => student.applicationStatus === "Approved")
+    .length;
+}
+
+function renderStudents(students) {
+  students.forEach((student) => {
+    const studentItem = document.createElement("li");
+    studentItem.classList.add("student");
+    const studentName = document.createElement("h3");
+    studentName.classList.add("student__name");
+    const applicationStatus = document.createElement("p");
+    applicationStatus.classList.add(
+      "student__status",
+      student.applicationStatus.toLowerCase()
+    );
+
+    const bursaryAmount = document.createElement("p");
+    bursaryAmount.classList.add("student__amount");
+
+    studentName.textContent = `${student.firstName} ${student.lastName}`;
+    applicationStatus.textContent = student.applicationStatus;
+    bursaryAmount.textContent = `R${student.amount}`;
+    studentItem.appendChild(studentName);
+    studentItem.appendChild(applicationStatus);
+    studentItem.appendChild(bursaryAmount);
+    universityStudents.appendChild(studentItem);
+  });
+}
