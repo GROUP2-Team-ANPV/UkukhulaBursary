64a9281072dbb383fc8e6c123189effc9cf25121
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-27 07:46:18 +0200

    add functionality to get the University Data

---
+++ university/university_data.js
@@ -0,0 +1,43 @@
+const universityStudents = document.querySelector(".students");
+
+async function getUniversityData(id) {
+  const response = await fetch(
+    `http://localhost:5263/api/UniversityAdmin/GetUniversityAndTheirStudents?universityID=${id}`,
+    {
+      method: "GET",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+getUniversityData(1).then(
+  (data) =>
+    data.students.forEach((student) => {
+      const studentItem = document.createElement("li");
+      studentItem.classList.add("student");
+      const studentName = document.createElement("h3");
+      studentName.classList.add("student__name");
+      const applicationStatus = document.createElement("p");
+      applicationStatus.classList.add("student__status",student.applicationStatus.toLowerCase());
+      const bursaryAmount = document.createElement("p");
+      bursaryAmount.classList.add("student__amount");
+
+      studentName.textContent = `${student.firstName} ${student.lastName}`;
+      applicationStatus.textContent = student.applicationStatus;
+      bursaryAmount.textContent = student.amount;
+      studentItem.appendChild(studentName);
+      studentItem.appendChild(applicationStatus);
+      studentItem.appendChild(bursaryAmount);
+      universityStudents.appendChild(studentItem);
+      
+    })
+    
+);
