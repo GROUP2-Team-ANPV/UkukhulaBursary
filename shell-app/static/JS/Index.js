8902db84e1d9d3ab41b2ef1da47e76f9351712dd
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-02-28 10:03:41 +0200

    remove unused code

--- shell-app/static/JS/Index.js
+++ shell-app/static/JS/Index.js
@@ -71,36 +71,30 @@ menuBar.addEventListener("click", function () {
   sidebar.classList.toggle("hide");
 });
 
-const searchButton = document.querySelector(
-  "#header nav form .form-input button"
-);
-const searchButtonIcon = document.querySelector(
-  "#header nav form .form-input button .bx"
-);
-const searchForm = document.querySelector("#header nav form");
+// const searchButton = document.querySelector(
+//   "#header nav form .form-input button"
+// );
+// const searchButtonIcon = document.querySelector(
+//   "#header nav form .form-input button .bx"
+// );
+// const searchForm = document.querySelector("#header nav form");
 
-searchButton.addEventListener("click", function (e) {
-  if (window.innerWidth < 576) {
-    e.preventDefault();
-    searchForm.classList.toggle("show");
-    if (searchForm.classList.contains("show")) {
-      searchButtonIcon.classList.replace("bx-search", "bx-x");
-    } else {
-      searchButtonIcon.classList.replace("bx-x", "bx-search");
-    }
-  }
-});
+// searchButton.addEventListener("click", function (e) {
+//   if (window.innerWidth < 576) {
+//     e.preventDefault();
+//     searchForm.classList.toggle("show");
+//     if (searchForm.classList.contains("show")) {
+//       searchButtonIcon.classList.replace("bx-search", "bx-x");
+//     } else {
+//       searchButtonIcon.classList.replace("bx-x", "bx-search");
+//     }
+//   }
+// });
 
 if (window.innerWidth < 768) {
   sidebar.classList.add("hide");
-} else if (window.innerWidth > 576) {
-  searchButtonIcon.classList.replace("bx-x", "bx-search");
-  searchForm.classList.remove("show");
-}
-
-window.addEventListener("resize", function () {
-  if (this.innerWidth > 576) {
-    searchButtonIcon.classList.replace("bx-x", "bx-search");
-    searchForm.classList.remove("show");
-  }
-});
+} 
+// else if (window.innerWidth > 576) {
+//   searchButtonIcon.classList.replace("bx-x", "bx-search");
+//   searchForm.classList.remove("show");
+// }
