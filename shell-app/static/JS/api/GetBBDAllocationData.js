0937ee8a0cdc4cf7885e41b60a6f5cbb075f13d2
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-02 12:28:01 +0200

    aff functionality to get bbd allocations data

---
+++ shell-app/static/JS/api/GetBBDAllocationData.js
@@ -0,0 +1,21 @@
+import getAllStudents from "../helpers/get_all_students.js";
+import getBBDFunds from "../helpers/get_bbd_funds.js";
+import getAllUniversities from "../helpers/get_all_universities.js";
+
+async function getBBDAllocationsData() {
+  const allocations = await getBBDFunds(
+    "http://localhost:5263/api/BBDAdmin/GetAllBBDFunds"
+  );
+
+  const universities = await getAllUniversities(
+    "http://localhost:5263/api/BBDAdmin/GetAllUniversities"
+  );
+
+  const students = await getAllStudents(
+    "http://localhost:5263/api/UniversityAdmin/GetAllFundRequests"
+  );
+
+  return { allocations, universities, students };
+}
+
+export default getBBDAllocationsData;
