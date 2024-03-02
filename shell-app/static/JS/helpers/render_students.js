121ecaa585b0dfca2ea9521fa3d50342b8e30ed8
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-02 12:33:30 +0200

    add functionality to render the students

---
+++ shell-app/static/JS/helpers/render_students.js
@@ -0,0 +1,22 @@
+function renderStudents(allStudents) {
+  const studentsList = allStudents.map((student) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("student");
+
+    const studentName = document.createElement("h3");
+    studentName.classList.add("student__name");
+    studentName.textContent = `${student.firstName} ${student.lastName}`;
+
+    const university = document.createElement("p");
+    university.classList.add("student__university");
+    university.textContent = student.university;
+
+    listItem.append(studentName, university);
+
+    return listItem;
+  });
+
+  return studentsList;
+}
+
+export default renderStudents;
