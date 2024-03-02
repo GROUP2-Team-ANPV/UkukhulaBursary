8fccf6d218f110ff1d74afd37948f158cd1a1f88
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-02 12:10:51 +0200

    add path to bbd dashboard to be served via node

--- shell-app/static/JS/Index.js
+++ shell-app/static/JS/Index.js
@@ -1,5 +1,6 @@
 //import LoginView from "./views/LoginView.js";
 import UniversityApplicationView from "./views/UniversityApplicationView.js";
+import BBDAdminView from "./views/BBDAdminView.js";
 import StudentApplicationView from "./views/StudentApplicationView.js";
 import UniversityDashboardView from "./views/UniversityDashboardView.js";
 
@@ -11,12 +12,10 @@ const navigateTo = (url) => {
 const router = async () => {
   const routes = [
     { path: "/", view: UniversityDashboardView },
+    { path: "/admin", view: BBDAdminView },
     { path: "/universityappliction", view: UniversityApplicationView },
     { path: "/studentapplication", view: StudentApplicationView },
-   // { path: "/login", view: LoginView },
-    
-    
-  
+    // { path: "/login", view: LoginView },
   ];
 
   // Test each route for potential match
@@ -38,49 +37,36 @@ const router = async () => {
   const view = new match.route.view();
   const allSideMenu = document.querySelectorAll("#sidebar  li a");
 
+  allSideMenu.forEach((item) => {
+    const li = item.parentElement;
+    const url = window.location.href.toString();
+    const aurl = item.getAttribute("href").toString();
 
-allSideMenu.forEach((item) => {
-  const li = item.parentElement;
-  const url = window.location.href.toString()
-  const aurl =item.getAttribute("href").toString()
+    if (url.includes(aurl) && aurl.includes(url.slice(url.length - 1))) {
+      li.classList.add("active");
+      item.firstChild;
+    } else {
+      li.classList.remove("active");
+    }
+  });
 
-  
-  if((url.includes(aurl) && aurl.includes(url.slice(url.length-1)) )){
-    
-    li.classList.add("active")
-    item.firstChild
-  }else{
-    li.classList.remove("active");
-  }
-  
-  
- 
-});
-
-  document.querySelector("#content").innerHTML= await view.getHtml();
+  document.querySelector("#content").innerHTML = await view.getHtml();
   await view.getJS();
-
-  
-  
-  
 };
 
 window.addEventListener("popstate", router);
 document.addEventListener("DOMContentLoaded", () => {
-
   document.body.addEventListener("click", (e) => {
     const targetLink = e.target.closest("[data-link]");
 
     if (targetLink) {
       e.preventDefault();
-     
+
       navigateTo(targetLink.href);
     }
   });
   router();
 });
-
-
 
 const menuBar = document.querySelector("#header nav .bx.bx-menu");
 const sidebar = document.getElementById("sidebar");
@@ -89,14 +75,9 @@ menuBar.addEventListener("click", function () {
   sidebar.classList.toggle("hide");
 });
 
-
-
-
-
 const head = document.querySelector("head");
-  
-    const cssLink = document.createElement("link");
-    cssLink.setAttribute("id", "content-css")
-    cssLink.setAttribute("rel", "stylesheet"); 
-    head.appendChild(cssLink);
 
+const cssLink = document.createElement("link");
+cssLink.setAttribute("id", "content-css");
+cssLink.setAttribute("rel", "stylesheet");
+head.appendChild(cssLink);
