2ad3a887a18a0214395dfaccbc32b7162bb1c99d
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-02 13:48:50 +0200

    move HODs rendering function to helpers

--- shell-app/static/JS/views/UniversityDashboardView.js
+++ shell-app/static/JS/views/UniversityDashboardView.js
@@ -2,6 +2,7 @@ import AbstractViews from "./AbstractViews.js";
 import { getUniversityData } from "../api/GetUniversityData.js";
 import populateStudentModal from "../helpers/populate_student_info.js";
 import formatMoney from "../helpers/format_money.js";
+import renderHODs from "../helpers/render_HODs.js";
 
 export default class extends AbstractViews {
   constructor() {
@@ -16,19 +17,20 @@ export default class extends AbstractViews {
     );
     return html;
   }
+
   async getJS() {
     const universityStudents = document.querySelector(".students");
     const fundedStudents = document.querySelector(".funded");
     const hods = document.querySelector(".hods");
     const fundsBalance = document.querySelector(".funds__balance");
     const usedFunds = document.querySelector(".used");
-    const studentInfo = document.querySelector(".student__info");
+    const studentInfoContainer = document.querySelector(".student__info");
     const studentNameContainer = document.querySelector(".name");
 
     getUniversityData(1).then((data) => {
-      renderStudents(data.students);
+      renderStudents(data.students, studentInfoContainer);
       fundedStudents.textContent = getFundedStudents(data.students);
-      renderHODs(data.headOfDepartment);
+      hods.append(...renderHODs(data.headOfDepartment));
 
       // year to be replaced with current year
       const funds = data.fundAllocation.filter(
@@ -63,7 +65,7 @@ export default class extends AbstractViews {
       documentBody.classList.remove("no-scroll");
     });
 
-    function renderStudents(students) {
+    function renderStudents(students, studentInfoContainer) {
       students.forEach((student) => {
         const studentItem = document.createElement("li");
         studentItem.classList.add("student");
@@ -90,35 +92,14 @@ export default class extends AbstractViews {
         studentItem.addEventListener("click", () => {
           studentInfoModal.style.transitionDelay = "0s";
 
-          studentInfo.textContent = "";
+          studentInfoContainer.textContent = "";
           studentNameContainer.textContent = `${student.firstName} ${student.lastName}`;
-          studentInfo.append(...populateStudentModal(student));
+          studentInfoContainer.append(...populateStudentModal(student));
 
           studentInfoModal.classList.add("show");
           window.scrollTo(0, 0);
           documentBody.classList.add("no-scroll");
         });
-      });
-    }
-
-    function renderHODs(universityHODs) {
-      universityHODs.forEach((hod) => {
-        const hodItem = document.createElement("li");
-        hodItem.classList.add("hod");
-        const hodName = document.createElement("h3");
-        hodName.classList.add("hod__name");
-        const hodEmail = document.createElement("p");
-        hodEmail.classList.add("email");
-        const email = document.createElement("a");
-        email.href = `mailto:${hod.email}`;
-        email.textContent = hod.email;
-
-        hodName.textContent = `${hod.firstName} ${hod.lastName}`;
-        hodEmail.textContent = hod.email;
-        hodItem.appendChild(hodName);
-        hodItem.appendChild(hodEmail);
-        hodItem.appendChild(email);
-        hods.appendChild(hodItem);
       });
     }
 
