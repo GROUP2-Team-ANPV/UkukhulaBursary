2def0ead83525bb233c4d54419b60df24aa891df
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-03 17:46:01 +0200

    remove testing code

--- shell-app/static/JS/JwtDecoder.js
+++ shell-app/static/JS/JwtDecoder.js
@@ -1,26 +1,32 @@
+export function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
-export function parseJwt (token) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    
-     const userdata = JSON.parse(jsonPayload)
-     const userRole  = userdata["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]
-     const userUniversity= userdata["http://schemas.microsoft.com/ws/2008/06/identity/claims/groupsid"]
-     console.log(userUniversity)
+  const userdata = JSON.parse(jsonPayload);
+  const userRole =
+    userdata["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+  const userUniversity =
+    userdata[
+      "http://schemas.microsoft.com/ws/2008/06/identity/claims/groupsid"
+    ];
 
-     if( userRole =="University Admin"){
-        sessionStorage.setItem("universityID",userUniversity)
-        console.log(sessionStorage.getItem("universityID"))
-     }
-      const usertimeStamp =userdata["exp"]
-      const expirationDate = new Date(usertimeStamp * 1000);
-      if (expirationDate.getMinutes ==0) {
-        console.log("User session is don resign in")
-        sessionStorage.clear();
-         window.location.href = "/login";
-      }
-    return  userRole
+  if (userRole == "University Admin") {
+    sessionStorage.setItem("universityID", userUniversity);
+  }
+  const usertimeStamp = userdata["exp"];
+  const expirationDate = new Date(usertimeStamp * 1000);
+  if (expirationDate.getMinutes == 0) {
+    sessionStorage.clear();
+    window.location.href = "/login";
+  }
+  return userRole;
 }
