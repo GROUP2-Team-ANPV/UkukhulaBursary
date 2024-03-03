bfd2a65b47dbb211548db075e5d620996c9ff298
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-03 09:24:52 +0200

    add functionality to turn application status to a form field when the application status is review

--- shell-app/static/JS/helpers/populate_student_info.js
+++ shell-app/static/JS/helpers/populate_student_info.js
@@ -1,33 +1,59 @@
+import getApplicationConstants from "../api/GetApplicationConstants.js";
 import formatMoney from "./format_money.js";
+import populateStatusSelect from "./populate_status_select.js";
 
 function populateStudentModal(student) {
   const studentInfo = [];
+  const form = document.createElement("form");
+  form.classList.add("info");
 
   for (let [key, value] of Object.entries(student)) {
     const label = key.replace(/([A-Z])/g, " $1").toLowerCase();
 
     if (
-      label != "student i d" &&
+      !label.includes("i d") &&
       label !== "first name" &&
       label !== "last name"
     ) {
+      let infoLabel;
+      let infoValue;
+
       const infoItem = document.createElement("li");
       infoItem.classList.add("info");
-      const infoLabel = document.createElement("p");
-      infoLabel.classList.add("label");
-      const infoValue = document.createElement("p");
-      infoValue.classList.add("value");
+      if (label === "status" && value === "Review") {
+        infoLabel = document.createElement("label");
+        infoLabel.setAttribute("for", "status");
+        infoLabel.textContent = "status";
+        infoValue = document.createElement("select");
+        infoValue.name = "status";
+        infoValue.id = "status";
+        infoValue.required = true;
+        const option = document.createElement("option");
+        option.value = "0";
+        option.textContent = "Select Status";
+        infoValue.appendChild(option);
 
-      infoLabel.textContent = label;
+        getApplicationConstants().then(({ status }) => {
+          infoValue.append(...populateStatusSelect(status));
+        });
+      } else {
+        infoLabel = document.createElement("p");
+        infoLabel.classList.add("label");
+        infoValue = document.createElement("p");
+        infoValue.classList.add("value");
 
-      if (label === "amount") {
-        value = formatMoney(value);
+        infoLabel.textContent = label;
+
+        if (label === "amount") {
+          value = formatMoney(value);
+        }
+        if (label.includes("date") && value !== null) {
+          value = value.split("T")[0];
+        }
+
+        infoValue.textContent = value;
       }
-      if (label === "birth date") {
-        value = value.split("T")[0];
-      }
 
-      infoValue.textContent = value;
       infoItem.appendChild(infoLabel);
       infoItem.appendChild(infoValue);
       studentInfo.push(infoItem);
