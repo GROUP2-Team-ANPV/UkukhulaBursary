a07dc3c2dda7775f25c6106cc997c2ff1d8cb676
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-03 09:16:45 +0200

    add functionality to get status

--- shell-app/static/JS/api/GetApplicationConstants.js
+++ shell-app/static/JS/api/GetApplicationConstants.js
@@ -2,6 +2,7 @@ import getAllUniversities from "../helpers/get_all_universities.js";
 import getDepartments from "../helpers/get_departments.js";
 import getEthnicity from "../helpers/get_ethnicity.js";
 import getGender from "../helpers/get_gender.js";
+import getStatus from "../helpers/get_status.js";
 
 async function getApplicationConstants() {
   const departments = await getDepartments(
@@ -20,7 +21,11 @@ async function getApplicationConstants() {
     "http://localhost:5263/api/ConstantTables/GetRace"
   );
 
-  return { departments, universities, gender, race };
+  const status = await getStatus(
+    "http://localhost:5263/api/ConstantTables/GetStatus"
+  );
+
+  return { departments, universities, gender, race, status };
 }
 
 export default getApplicationConstants;
