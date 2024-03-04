89de76c0c17c1ddd138b382ffdac9dd406c8d360
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-04 15:35:25 +0200

    fix foruse correct form

--- shell-app/static/JS/UniversityApplication.js
+++ shell-app/static/JS/UniversityApplication.js
@@ -1,21 +1,56 @@
-import { AddUniversity } from "./api/AddUniversity.js";
-
-export function UniversityApplicationScript(){
+export function UniversityApplicationScript() {
   const universityForm = document.querySelector(".university-form");
 
-  universityForm.addEventListener("submit", handleAddUniversity);
-
   async function handleAddUniversity(event) {
+    const feedbackContainer = document.querySelector(".feedback");
+    const feedbackHeading = document.querySelector(".feedback__heading");
+    const feedbackMessage = document.querySelector(".feedback__message");
     event.preventDefault();
     const universityData = {};
     const formData = new FormData(universityForm);
-    
+
     for (const [key, value] of formData) {
       universityData[key] = value;
     }
-    
-    await AddUniversity(universityData)
-    universityForm.reset();
-}
-}
 
+    try {
+      const response = await fetch(
+        "http://localhost:5263/api/BBDAdmin/AddUniversity",
+        {
+          method: "POST",
+          mode: "cors",
+          cache: "no-cache",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(universityData),
+        }
+      );
+
+      const data = response.statusText;
+
+      if (data === "OK") {
+        universityForm.reset();
+        feedbackHeading.textContent = "Success";
+        feedbackMessage.textContent = "University added successfully";
+
+        feedbackContainer.style.backgroundColor = "var(--success)";
+      }
+    } catch (error) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent =
+        "An error occured while adding the University";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+    } finally {
+      feedbackContainer.classList.add("feedback--show");
+
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+    }
+  }
+  universityForm.addEventListener("submit", handleAddUniversity);
+}
