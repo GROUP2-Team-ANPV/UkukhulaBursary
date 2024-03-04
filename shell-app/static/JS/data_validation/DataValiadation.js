0712be42b3661402f39d8f57886090b2f73c0618
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-04 14:49:38 +0200

    make it clear that the functions return boolean values

--- shell-app/static/JS/data_validation/DataValiadation.js
+++ shell-app/static/JS/data_validation/DataValiadation.js
@@ -1,25 +1,20 @@
-function validatePhoneNumber(phoneNumber) {
+function isValidPhoneNumber(phoneNumber) {
   const phoneRegex = /^\d{10}$/;
   return phoneRegex.test(phoneNumber);
 }
 
-function validateEmail(email) {
+function isValidEmail(email) {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email);
 }
 
-function validateName(name) {
+function isValidName(name) {
   const nameRegex = /^[a-zA-Z]+$/;
   return nameRegex.test(name);
 }
 
-function validateOptionValue(value) {
+function isValidOptionValue(value) {
   return value !== "0";
 }
 
-export {
-  validatePhoneNumber,
-  validateEmail,
-  validateName,
-  validateOptionValue,
-};
+export { isValidPhoneNumber, isValidEmail, isValidName, isValidOptionValue };
