3ab1e5a109fc5ccd0b41f11346c9e0a9eaeec4d0
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-04 16:09:46 +0200

    add data validation

--- shell-app/static/JS/UniversityApplication.js
+++ shell-app/static/JS/UniversityApplication.js
@@ -1,3 +1,9 @@
+import {
+  isValidEmail,
+  isValidName,
+  isValidPhoneNumber,
+  isValidOptionValue,
+} from "./data_validation/DataValiadation.js";
 export function UniversityApplicationScript() {
   const universityForm = document.querySelector(".university-form");
 
@@ -11,6 +17,106 @@ export function UniversityApplicationScript() {
 
     for (const [key, value] of formData) {
       universityData[key] = value;
+    }
+
+    console.log(universityData);
+
+    if (!isValidEmail(universityData.email)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Invalid email address";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
+    }
+
+    if (!isValidPhoneNumber(universityData.phoneNumber)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Invalid phone number";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
+    }
+
+    if (!isValidName(universityData.universityName)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Invalid university name";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
+    }
+
+    if (!isValidName(universityData.firstName)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Invalid first name";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
+    }
+
+    if (!isValidName(universityData.lastName)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Invalid last name";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
+    }
+
+    if (!isValidOptionValue(universityData.provinceID)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Please select a province";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
+    }
+
+    if (!isValidOptionValue(universityData.departmentID)) {
+      feedbackHeading.textContent = "Error";
+      feedbackMessage.textContent = "Please select a department";
+      feedbackContainer.style.backgroundColor = "var(--danger)";
+      feedbackContainer.classList.add("feedback--show");
+      setTimeout(() => {
+        feedbackContainer.classList.remove("feedback--show");
+        feedbackHeading.textContent = "";
+        feedbackMessage.textContent = "";
+        feedbackContainer.style.backgroundColor = "";
+      }, 3000);
+      return;
     }
 
     try {
