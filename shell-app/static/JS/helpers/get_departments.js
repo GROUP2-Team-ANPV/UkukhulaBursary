da89321da03a35f0c5708a4fb83387bee0c680af
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-03-04 22:10:13 +0200

    Updated

--- shell-app/static/JS/helpers/get_departments.js
+++ shell-app/static/JS/helpers/get_departments.js
@@ -1,6 +1,11 @@
+const token = sessionStorage.getItem("token");
 async function getDepartments(url) {
   const response = await fetch(url, {
     method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    },
   });
   const data = await response.json();
   return data;
